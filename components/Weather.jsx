8f45376fe03008de2f51e0204e8311447e42cd8f
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-07-02 17:24:50 +0300

    change temperature

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -16,14 +16,17 @@ const Weather = ({ data }) => {
             width='100'
             height='100'
           />
-          <p className='text-2xl'>{data.weather[0].main}</p>
+          <p className='text-2xl text-amber-100'>{data.weather[0].main}</p>
         </div>
-        <p className='text-9xl'>{temperatureCelsius}&#176;C</p>
-        <p className='text-9xl'>{temperatureFahrenheit}&#176;F</p>
+        <div className='flex justify-between text-center pl-9 '>
+            <p className='text-9xl text-green-200'>{temperatureCelsius}&#176;C</p>
+            <p className='text-9xl pl-8 text-pink-200'>{temperatureFahrenheit}&#176;F</p>
+        </div>
+
       </div>
       {/* Bottom */}
 
-<div className='bg-black/50 relative p-8 rounded-md'>
+<div className='bg-black/40 relative p-8 rounded-md'>
     <p className='text-2xl text-center pb-6'>Weather in {data.name}</p>
     <div className='flex justify-between text-center'>
         <div>

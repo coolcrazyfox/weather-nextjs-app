4837a0b4c8e305c0fc7a3d20dfc49fea8d520d95
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-07-02 17:47:26 +0300

    change temperature

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -4,23 +4,25 @@ import React from 'react';
 const Weather = ({ data }) => {
   console.log(data);
   let temperatureCelsius = Math.floor(((data.main.temp.toFixed(0))-32)*5/9)
-    let temperatureFahrenheit = data.main.temp.toFixed(0)
+  let temperatureFahrenheit = data.main.temp.toFixed(0)
+  let temperatureFeelsCelsius = Math.floor(((data.main.feels_like.toFixed(0))-32)*5/9)
+  let temperatureFeelsFahrenheit = data.main.feels_like.toFixed(0)
   return (
-    <div className='relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4 text-gray-300 z-10'>
+    <div className='relative flex flex-col justify-between max-w-[600px] w-full h-[85vh] m-auto p-4 text-gray-300 z-10'>
       {/* Top */}
       <div className='relative flex justify-between pt-12 '>
         <div className='flex flex-col items-center '>
           <Image
             src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
             alt='/'
-            width='100'
-            height='100'
+            width='130'
+            height='130'
           />
-          <p className='text-2xl text-amber-100'>{data.weather[0].main}</p>
+          <p className='text-4xl text-emerald-50 '>{data.weather[0].main}</p>
         </div>
-        <div className='flex justify-between text-center pl-9 '>
-            <p className='text-9xl text-green-200'>{temperatureCelsius}&#176;C</p>
-            <p className='text-9xl pl-8 text-pink-200'>{temperatureFahrenheit}&#176;F</p>
+        <div className='flex flex-col '>
+            <p className='text-8xl text-gray-300 '>{temperatureCelsius}&#176;C</p>
+            <p className='text-8xl text-gray-300'>{temperatureFahrenheit}&#176;F</p>
         </div>
 
       </div>
@@ -30,7 +32,11 @@ const Weather = ({ data }) => {
     <p className='text-2xl text-center pb-6'>Weather in {data.name}</p>
     <div className='flex justify-between text-center'>
         <div>
-            <p className='font-bold text-2xl'>{data.main.feels_like.toFixed(0)}&#176;</p>
+            <p className='font-bold text-2xl'>{temperatureFeelsCelsius}&#176;C</p>
+            <p className='text-xl'>Feels Like</p>
+        </div>
+        <div>
+            <p className='font-bold text-2xl'>{temperatureFeelsFahrenheit}&#176;F</p>
             <p className='text-xl'>Feels Like</p>
         </div>
         <div>
